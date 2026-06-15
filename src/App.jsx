7474df0e8786ab2db5a124eb1377cefff30b1620
amanbankey <amanbankey8@gmail.com>
2026-06-15 11:37:55 +0530

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-import Layout from './layouts/Layout'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+import Layout from "./layouts/Layout";
 import { Routes, Route } from "react-router-dom";
-import Home from './pages/Home'
-import NiwasAI from './pages/NiwasAI'
-import About from './pages/About'
-import AgentCorner from './pages/AgentCorner'
+import Home from "./pages/Home";
+import NiwasAI from "./pages/NiwasAI";
+import About from "./pages/About";
+import AgentCorner from "./pages/AgentCorner";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-   
-
-   <Routes>
+      <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
 
-          <Route path='niwas-ai' element={<NiwasAI/>} />
-        <Route path='about' element={<About/>} />
-            <Route path='agent-corner' element={<AgentCorner/>} />
-          {/* <Route path='admin-dashboard' element={<AdminDashboard/>} />
-
-          <Route path='admin-login' element={<AdminLogin/>} />
-
-           <Route path='b2b-dashboard' element={<B2BDashboard/>} />
-            <Route path='user-dashboard' element={<UserDashboard/>} /> */}
-
+          <Route path="niwas-ai" element={<NiwasAI />} />
+          <Route path="about" element={<About />} />
+          <Route path="agent-corner" element={<AgentCorner />} />
           
-
         </Route>
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
